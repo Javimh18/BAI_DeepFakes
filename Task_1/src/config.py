fbbaca245f49bf0d6f57c2e8d038f532c8214163
Javier Muñoz <javier.munnozharo@estudiante.uam.es>
2024-03-16 09:47:49 +0100

    changed values for config file

--- Task_1/src/config.py
+++ Task_1/src/config.py
@@ -17,9 +17,12 @@ CNN_MODEL_NAME = 'ResNet18' # Name of the model
 CNN_WEIGHT_INIT = 'Xavier_Normal' # Init -> Xavier_Normal, Xavier_Uniform (tested), He
 
 # VAE loss weight parameters
+VAE_BATCH_SIZE = 64
 VAE_ALPHA = 5e4
 VAE_EPOCHS = 500
-VAE_LR = 1e-4 # lr = 0.01
+VAE_LR = 5e-2 # lr = 0.01
 VAE_WEIGHT_DECAY = 5e-7
 VAE_BETAS = (0.95, 0.999)
-VAE_REG_PAR = 1e-5 # reg_par = 1e-5
+VAE_REG_PAR = 0# 5e-3 at first, but after few epochs should be increasing to give the KL Loss more importance
+VAE_IM_HEIGHT = 64
+VAE_IM_WIDTH = 64
